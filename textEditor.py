42a167cf868d2b0c477e03efd69d5ed5a10b8648
Seanong011 <keifferong01@gmail.com>
2025-02-07 09:02:42 +0800

    Status Update and Scrollbar

--- textEditor.py
+++ textEditor.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from tkinter.filedialog import asksaveasfile, askopenfile
 from tkinter.messagebox import showerror
 
@@ -8,41 +9,75 @@ filename = None # Initialize filename
 
 def newFile():
     global filename
-    filename = "Untitled"
+    filename = None
     text.delete(0.0, END) # 0th Row and 0th Column respectively; from (0,0) to END 
 
 def saveFile():
     global filename
-    t = text.get(0.0, END) # Retrieves text from start to END
-    f = open(filename, 'w') # Opens the file with the corresponding filename 
-    f.write(t)
-    f.close()
+    if filename is None:
+        saveAs()
+    else:
+        try:
+            with open(filename, 'w') as f:
+                f.write(text.get(0.0, END))
+        except:
+            showerror(title="Error", message="Unable to save file.")
 
 def saveAs():
+    global filename
     f = asksaveasfile(mode='w', defaultextension='.txt')
-    t = text.get(0.0, END)
+    if f is None:
+        return
+    filename = f.name
     try:
-        f.write(t.rstrip()) # Removes whitespace below the end of text
+        f.write(text.get(0.0, END).rstrip())
+        f.close()
     except:
         showerror(title="Error", message="Unable to save file.")
 
 def openFile():
+    global filename
     f = askopenfile(mode='r')
-    t = f.read()
+    if f is None:
+        return
+    filename = f.name
     text.delete(0.0, END)
-    text.insert(0.0, t)
+    text.insert(0.0, f.read())
 
+def update_cursor_position(event=None):
+    row, col = text.index(INSERT).split('.')
+    status_bar.config(text=f"Line: {row} | Column: {int(col) + 1}")
+
+# Applicaton
 root = Tk()
 root.title("Basic Python Text Editor")
-root.minsize(width=400, height=400)
-root.maxsize(width=400, height=400)
+root.geometry("600x400")
 
-text = Text(root, width=400, height=400)
-text.pack() # Display textbox
+# Widget & Scrollbar frame
+frame = Frame(root)
+frame.pack(fill=BOTH, expand=TRUE)
+
+# Text Widget
+text = Text(frame, wrap=NONE, undo=TRUE)
+text.pack(side=LEFT, fill=BOTH, expand=TRUE)
+
+# Scrollbar (Vertical)
+scrollbar = Scrollbar(frame, command=text.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
+text.config(yscrollcommand=scrollbar.set)
+
+# Scrollbar (Horizontal)
+scrollbar_x = Scrollbar(root, command=text.xview, orient=HORIZONTAL)
+scrollbar_x.pack(side=BOTTOM, fill=X)
+text.config(xscrollcommand=scrollbar_x.set)
+
+# Statusbar
+status_bar = Label(root, text="Line: 1 | Column: 1", anchor = 'w')
+status_bar.pack(fill=X, side=BOTTOM)
 
 # Menubar
 menubar = Menu(root)
-filemenu = Menu(menubar)
+filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="New", command=newFile)
 filemenu.add_command(label="Open", command=openFile)
 filemenu.add_command(label="Save", command=saveFile)
@@ -52,4 +87,7 @@ filemenu.add_command(label="Quit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 root.config(menu=menubar)
+text.bind("<KeyRelease>", update_cursor_position)
+text.bind("<ButtonRelease>", update_cursor_position)
+
 root.mainloop()
